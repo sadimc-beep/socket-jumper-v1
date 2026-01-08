6763e7966feea5060d79f57715b06a8e05ac1bf4
sadimc-beep <sadi.mc@gmail.com>
2026-01-08 18:27:10 +0600

    Fix settings.py to handle empty DATABASE_URL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,8 +97,9 @@ DATABASES = {
 }
 
 # Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+if os.environ.get('DATABASE_URL'):
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 
 AUTH_USER_MODEL = 'core.User'
